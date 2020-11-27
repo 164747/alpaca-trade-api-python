9dd33ff41577070f3a1c3c036ddd320f5a479f4e
David Jacquet <jacquet.david@gmail.com>
2020-11-27 21:41:47 +0100

    buy/sell loop

--- alpaca_trade_api/models/aux.py
+++ alpaca_trade_api/models/aux.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import typing
 import uuid
-from decimal import Decimal
 from enum import Enum
 
 from pydantic import BaseModel, Field
@@ -31,5 +30,5 @@ class OrderBase(BaseModel):
     client_order_id: str = Field(default_factory=lambda: str(uuid.uuid4()))
     qty: int
     time_in_force: OrderTimeInFore = OrderTimeInFore.DAY
-    limit_price: typing.Optional[Decimal]
-    stop_price: typing.Optional[Decimal] = None
+    limit_price: typing.Optional[float]
+    stop_price: typing.Optional[float] = None
--- alpaca_trade_api/models/rest_models.py
+++ alpaca_trade_api/models/rest_models.py
@@ -2,24 +2,20 @@ from __future__ import annotations
 
 import datetime
 import typing
-from decimal import Decimal
 from enum import Enum
 
-from pydantic import BaseModel, Field
+from pydantic import Field
 
 from alpaca_trade_api.models import aux
 
-# _T = typing.TypeVar('_T')
-_T = typing.ClassVar
 
 class OrderSide(Enum):
     BUY = 'buy'
     SELL = 'sell'
 
 
-
 class OrderReplace(aux.OrderBase):
-    trail: typing.Optional[Decimal] = None
+    trail: typing.Optional[float] = None
 
 
 class OrderPlace(aux.OrderBase):
@@ -28,11 +24,11 @@ class OrderPlace(aux.OrderBase):
     order_type: str = Field(alias='type', default=aux.OrderType.LIMIT)
     side: OrderSide
     time_in_force: aux.OrderTimeInFore = aux.OrderTimeInFore.DAY
-    limit_price: typing.Optional[Decimal] = None
-    stop_price: typing.Optional[Decimal] = None
+    limit_price: typing.Optional[float] = None
+    stop_price: typing.Optional[float] = None
     extended_hours: bool = False
-    trail_price: typing.Optional[Decimal] = None
-    trail_percent: typing.Optional[Decimal] = None
+    trail_price: typing.Optional[float] = None
+    trail_percent: typing.Optional[float] = None
     order_class: aux.OrderClass = aux.OrderClass.SIMPLE
     stop_loss: typing.Optional[aux.StopLoss] = None
 
@@ -53,6 +49,8 @@ class Order(OrderPlace):
     filled_qty: int
     status: str
     legs: typing.Optional[typing.List[Order]] = None
-    hwm: Decimal
+    hwm: float
 
-
+    @property
+    def unmatched_qty(self) -> int:
+        return self.qty - self.filled_qty
